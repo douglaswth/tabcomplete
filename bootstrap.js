47a0fac3d351fd87d9463ca38dbb2c4f674f355d
Douglas William Thrift <douglas@douglasthrift.net>
2011-06-20 22:46:40 -0700

    It's kind of like progress...

--- bootstrap.js
+++ bootstrap.js
@@ -1,12 +1,74 @@
-/*
- */
+// Douglas Thrift
+//
+// Tab Complete
+//
+// bootstrap.js
+
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Tab Complete.
+ *
+ * The Initial Developer of the Original Code is
+ * Douglas Thrift <douglas@douglasthrift.net>.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+Components.utils.import("resource:///modules/imServices.jsm");
+
+let observer = {
+    observe: function(subject, topic, data)
+    {
+        if (topic != "conversation-loaded")
+            return;
+
+        this.onConversationLoaded(subject);
+    },
+    onConversationLoaded: function(subject)
+    {
+        Services.console.logStringMessage(subject.tagName);
+    },
+};
 
 function startup(data, reason)
 {
+    Services.obs.addObserver(observer, "conversation-loaded", false);
+    //Services.cmd.listCommandsForConversation
+    //Services.console.logStringMessage
 }
 
 function shutdown(data, reason)
 {
+    Services.obs.removeObserver(observer, "conversation-loaded");
 }
+
+function install(data, reason) {}
+function uninstall(data, reason) {}
 
 // vim: expandtab
